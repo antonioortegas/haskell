af90b050ce9b58b6d8c429f24f46c42b9057a5ca
antonioortegas <antonioortega@uma.es>
2023-11-03 12:18:21 +0100

    tree functiones

--- tema4.hs
+++ tema4.hs
@@ -20,4 +20,23 @@ sizeT (Node x ts) = 1 + sum (map sizeT ts)
 
 sumT :: (Num a) => Tree a -> a
 sumT Empty = 0
-sumT (Node x ts) = x + sum (map sumT ts) 
+sumT (Node x ts) = x + sum (map sumT ts)
+
+heightT :: Tree a -> Int
+heightT Empty = 0
+heightT (Node x []) = 1
+heightT (Node x ts) = 1 + maximum (map heightT ts)
+
+containsT :: Eq a => a -> Tree a -> Bool
+containsT x Empty = False
+containsT x (Node y ts) = x == y || or [containsT x t | t <- ts]
+
+atLevelT :: Int -> Tree a -> [a]
+atLevelT _ Empty = []
+atLevelT 0 (Node x ts) = [x]
+atLevelT n (Node x ts) = concat [atLevelT (n-1) t | t <- ts]
+
+leafsT :: Tree a -> [a]
+leafsT Empty = []
+leafsT (Node x []) = [x]
+leafsT (Node x ts) = concat [leafsT t | t <- ts]
